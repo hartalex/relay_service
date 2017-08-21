766447166501fc71264278e68b6587672506a2ab
Alex Hart <alex@hartcode.com>
2017-08-20 20:23:33 -0500

    Add arg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const wpi = require('wiringpi-node')
 
 var pins = [0, 2]
 var currentPin = 0
+var mode = process.argv[2]
 
 wpi.wiringPiSetupGpio()
 for (var setupPin in pins) {
@@ -12,10 +13,17 @@ for (var setupPin in pins) {
     console.log('Pin ' + currentPin + 'is on')
   }
 }
-for (var pin in pins) {
-  if (pin === currentPin) {
-    wpi.digitalWrite(pin, 1)
-  } else {
-    wpi.digitalWrite(pin, 0)
+if (mode === 'on' && currentPin === 0 || mode === 'off' && currentPin === 2) {
+  // nothing to do
+  console.log('Already set')
+} else {
+  // turn off currentPin and turn on the other pin
+  console.log('switching')
+  for (var pin in pins) {
+    if (pin === currentPin) {
+      wpi.digitalWrite(pin, 1)
+    } else {
+      wpi.digitalWrite(pin, 0)
+    }
   }
 }
